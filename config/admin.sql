fb2443aa62bdf0507e74df96da918bd6ceec4c63
suraj <sdevgan007@gmail.com>
2020-08-30 11:12:14 +0530

    first commit

--- config/admin.sql
+++ config/admin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 23, 2020 at 12:00 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Host: localhost
+-- Generation Time: Aug 30, 2020 at 07:39 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `furniture`
+-- Database: `admin`
 --
 
 -- --------------------------------------------------------
@@ -93,6 +93,7 @@ CREATE TABLE `images` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `menu`
 --
@@ -136,37 +137,61 @@ INSERT INTO `mission` (`id`, `des`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(255) NOT NULL,
+  `order_name` varchar(255) NOT NULL,
+  `order_mobile` int(20) NOT NULL,
+  `order_state` varchar(255) NOT NULL,
+  `status_address` varchar(255) NOT NULL,
+  `client_id` int(255) NOT NULL,
+  `order_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_name`, `order_mobile`, `order_state`, `status_address`, `client_id`, `order_status`) VALUES
+(1, 'Santosh Dash', 987654321, 'punjab', 'ludhiana', 0, ''),
+(2, 'rahul', 984678367, 'punjab', 'Samrala', 0, 'confirm');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
-  `id` int(255) NOT NULL,
-  `product_name` varchar(255) NOT NULL,
-  `feature_img` text NOT NULL,
-  `cat_name` varchar(255) NOT NULL,
+  `PRODUCT_ID` int(255) NOT NULL,
+  `PRODUCT_NAME` varchar(255) NOT NULL,
+  `PRODUCT_IMAGE` text NOT NULL,
+  `CATEGORY` varchar(255) NOT NULL,
   `cat_id` int(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `des` longtext NOT NULL
+  `PRICE` varchar(255) NOT NULL,
+  `DISCOUNT_PRICE` varchar(10) NOT NULL,
+  `PRODUCT_DES` longtext NOT NULL,
+  `ATTRIBUTES` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `feature_img`, `cat_name`, `cat_id`, `price`, `des`) VALUES
-(27, 'Esprit Ruffle Shirt', '282.jpg', 'Sofa Set', 16, '500', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><p></p>'),
-(28, 'Only Check Trouser', '282.jpg', 'Sofa Set', 16, '1000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(29, '  Classic Trench Coat  ', '282.jpg', 'Bed Set', 16, '2500', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(30, '  Classic Trench Coat  ', '2898.jpg', 'Tables', 16, '3000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(31, ' Classic Trench Coat ', '3924.jpg', 'chairs', 16, '5000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(32, ' Classic Trench Coat ', '2811.jpg', 'chairs', 16, '12000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(33, ' Classic Trench Coat ', '2057.jpg', 'chairs', 16, '25000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(34, ' Classic Trench Coat ', '1737.jpg', 'chairs', 16, '35000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(35, ' Classic Trench Coat ', '2362.jpg', 'chairs', 16, '55000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(36, ' Classic Trench Coat ', '3369.jpg', 'chairs', 16, '500000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(37, '  Classic Trench Coat  ', '2364.jpg', 'chairs', 16, '500000', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>'),
-(38, ' product 2 ', '282.jpg', 'chairs', 16, '100000', '<p>dfgdsf</p>'),
-(39, '    product 2    ', '4062.jpg', 'chairs', 16, '0000', '<p>dfdf fsdfsdf dsdf</p>');
+INSERT INTO `products` (`PRODUCT_ID`, `PRODUCT_NAME`, `PRODUCT_IMAGE`, `CATEGORY`, `cat_id`, `PRICE`, `DISCOUNT_PRICE`, `PRODUCT_DES`, `ATTRIBUTES`) VALUES
+(27, 'Esprit Ruffle Shirt', '282.jpg', 'Sofa Set', 16, '500', '', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><p></p>', ''),
+(33, '     Classic Trench Coat  vv   ', '2057.jpg', 'chairs', 16, '52', '', '<p>\r\n\r\n</p><p>00</p><p></p>', ''),
+(34, '                  kkkk                ', '1194.jpg', 'Sofa Set', 16, '44', '', '<p>ggg</p>', ''),
+(35, ' Classic Trench Coat ', '2362.jpg', 'chairs', 16, '55000', '', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>', ''),
+(36, ' Classic Trench Coat ', '3369.jpg', 'chairs', 16, '500000', '', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>', ''),
+(37, '  Classic Trench Coat  ', '2364.jpg', 'chairs', 16, '500000', '', '<p>\r\n\r\n</p><p>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p><div><div><br>\r\n\r\n</div></div><br><p></p>', ''),
+(38, ' product 2 ', '282.jpg', 'chairs', 16, '100000', '', '<p>dfgdsf</p>', ''),
+(40, 'sdjfkj', '1506.jpg', 'Sofa Set', 0, '22', '33', 'dfsdf', ''),
+(41, 'sdjfkj', '2297.jpg', 'Sofa Set', 0, '22', '33', 'dfsdf', ''),
+(42, '    sdjfkj  33  ', '3611.png', 'Bed Set', 0, '2233', '3246', 'dfsdf ddd 22', 'gm'),
+(43, 'sdjfkj', '2973.jpg', 'Sofa Set', 0, '22', '33', 'dfsdf', 'gm');
 
 -- --------------------------------------------------------
 
@@ -206,7 +231,7 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`id`, `des`, `slider1`, `slider2`, `slider3`) VALUES
-(1, '<p>dsfasdfasf</p><p><br></p><p>sdfasf</p><p>fgdfgbbbb&nbsp;dfsdfsdf</p><p>000</p>', '4867.jpg', '1301.jpg', '4105.jpg');
+(1, '<p>dsfasdfasf</p><p><br></p><p>sdfasf</p><p>fgdfgbbbb&nbsp;dfsdfsdf</p><p>000 11</p>', '4867.jpg', '1301.jpg', '4105.jpg');
 
 -- --------------------------------------------------------
 
@@ -234,6 +259,29 @@ INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `email`, `passw
 (1, '', '', 'superman', 'msm8829@gmail.com', '84d961568a65073a3bcf0eb216b2a576', '06fb7e3d9c63f70895e5942facdd0b39', '', ''),
 (2, '', '', 'admin', 'admin@admin.com', '827ccb0eea8a706c4c34a16891f84e7b', '17f2decb76103f2f168cffba60a299fc', '', ''),
 (7, 'Mandeep', 'singh', '', 'mandeepmehra7@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'active', 'b25f4937f861719e9bed207d02aca431');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlist`
+--
+
+CREATE TABLE `userlist` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` int(30) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `is_active` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlist`
+--
+
+INSERT INTO `userlist` (`id`, `name`, `mobile`, `state`, `address`, `is_active`) VALUES
+(2, 'rahul', 984678367, 'punjab', 'Samrala', '1'),
+(3, 'mandeep', 748273472, 'punjab', 'chd', '1');
 
 --
 -- Indexes for dumped tables
@@ -276,10 +324,16 @@ ALTER TABLE `mission`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`PRODUCT_ID`);
 
 --
 -- Indexes for table `service`
@@ -297,6 +351,12 @@ ALTER TABLE `slider`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlist`
+--
+ALTER TABLE `userlist`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -340,10 +400,16 @@ ALTER TABLE `mission`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `PRODUCT_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `service`
@@ -362,6 +428,12 @@ ALTER TABLE `slider`
 --
 ALTER TABLE `user`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `userlist`
+--
+ALTER TABLE `userlist`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
